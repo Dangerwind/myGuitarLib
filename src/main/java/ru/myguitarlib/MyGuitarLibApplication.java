128ee3b81a8b0e514ed0f5082cc6fa34181ded13
dangerwind <dangerwind@gmail.com>
2025-12-09 18:58:14 +0300

    first step

---
+++ src/main/java/ru/myguitarlib/MyGuitarLibApplication.java
@@ -0,0 +1,13 @@
+package ru.myguitarlib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyGuitarLibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyGuitarLibApplication.class, args);
+	}
+
+}
